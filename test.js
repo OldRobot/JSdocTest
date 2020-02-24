862290c998a18035b023f03585c2aba3bf6c6fef
Paul Smith <three_terriers@btinternet.com>
2020-02-24 09:53:30 +0000

    Added a new function

--- test.js
+++ test.js
@@ -8,3 +8,13 @@
 function areaOfRectangle(l,w){
     return l*w;
 }
+
+
+/**
+ * This function caculates the area of a circle
+ * @param {number} r - The radius of the circle
+ * @returns Will return the area of the circle to 6 dp.
+ */
+function areaOfCircle(r){
+    return 3.14159245 * r * r;
+}
