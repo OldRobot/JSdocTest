36f8f2f14009548ced8d9edaccc80699a683db62
Paul Smith <three_terriers@btinternet.com>
2020-02-24 09:44:26 +0000

    first file added

---
+++ test.js
@@ -0,0 +1,10 @@
+
+/**
+ * This function will calculate the area of a rectangle
+ * @param {number} l - Length of the Rectangle
+ * @param {number} w - Width of the rectangle
+ * @returns Will retun the area.
+ */
+function areaOfRectangle(l,w){
+    return l*w;
+}
